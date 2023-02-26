09d9e2ea98916ef75dcb8f896d57aa86103c1545
Lukasz Ozga <lukaszozga1324@gmail.com>
2023-02-26 00:27:54 +0000

    Correct broken references in user-ssh-key module

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "ssh_key" {
   value = {
-    "id"  = module.github_user_ssh_key.ssh_key.id
-    "url" = module.github_user_ssh_key.ssh_key.url
+    "id"  = module.github_user_ssh_key.ssh_key[*].id
+    "url" = module.github_user_ssh_key.ssh_key[*].url
   }
 }
--- terraform/user-ssh-key/variables.tf
+++ terraform/user-ssh-key/variables.tf
@@ -1,18 +1,18 @@
 variable "ssh_auth_enabled" {
   description = <<EOF
     "If set to true, creates SSH authentication key
-    in given Github account"
+    in given Github account."
     EOF
   type        = bool
 }
 
 variable "public_ssh_key_name" {
-  description = "Name of the SSH key file used for authentication"
+  description = "Name of the SSH key file used for authentication."
   type        = string
   default     = null
 }
 
-variable "github_user_ssh_key_file" {
-  description = "Path to the file containg public SSH key"
+variable "public_ssh_key_file_path" {
+  description = "Path to the file containg public SSH key."
   default     = null
 }
