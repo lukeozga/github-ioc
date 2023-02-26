731111d20ef53deddd528b559163b1dbd9db02cb
Lukasz Ozga <lukaszozga1324@gmail.com>
2023-02-26 17:57:32 +0000

    Remove unnecessary tenary operator

--- terraform/user-ssh-key/ssh.tf
+++ terraform/user-ssh-key/ssh.tf
@@ -1,5 +1,5 @@
 resource "github_user_ssh_key" "public_ssh_key" {
   count = var.ssh_auth_enabled ? 1 : 0
   title = var.public_ssh_key_name
-  key   = var.public_ssh_key_file_path != null ? file(var.public_ssh_key_file_path) : file("~/.ssh/id_rsa.pub")
+  key   = file(var.public_ssh_key_file_path)
 }
