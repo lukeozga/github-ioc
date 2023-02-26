bfe1cca4a24104cd9a294e0dfbd4b2b39acdb01d
Lukasz Ozga <lukaszozga1324@gmail.com>
2023-02-26 00:25:32 +0000

    Implement github-repo module

---
+++ terraform/github-repo/github-repo.tf
@@ -0,0 +1,64 @@
+locals {
+  general_repository_settings = {
+    has_issues                  = var.has_issues,
+    has_discussions             = var.has_discussions,
+    has_projects                = var.has_projects,
+    has_wiki                    = var.has_wiki,
+    is_template                 = var.is_template,
+    allow_merge_commit          = var.allow_merge_commit,
+    allow_squash_merge          = var.allow_squash_merge,
+    allow_rebase_merge          = var.allow_rebase_merge,
+    allow_auto_merge            = var.allow_auto_merge,
+    squash_merge_commit_title   = var.squash_merge_commit_title,
+    squash_merge_commit_message = var.squash_merge_commit_message,
+    merge_commit_title          = var.merge_commit_title,
+    merge_commit_message        = var.merge_commit_message,
+    delete_branch_on_merge      = var.delete_branch_on_merge,
+    auto_init                   = var.auto_init,
+    archived                    = var.archived,
+    archive_on_destroy          = var.archive_on_destroy
+  }
+  github_repos     = { for repo in var.standard_github_repositories : repo.name => merge(repo, local.general_repository_settings) }
+  pages_repos_list = [ for repo in var.standard_github_repositories : repo.name if repo.pages != null ]
+}
+
+resource "github_repository" "standard_repositories" {
+  for_each = local.github_repos
+
+  name                        = each.value.name
+  description                 = each.value.description
+  visibility                  = each.value.visibility
+  homepage_url                = each.value.homepage_url
+  gitignore_template          = each.value.gitignore_template
+  license_template            = each.value.license_template
+  has_issues                  = each.value.has_issues
+  has_discussions             = each.value.has_discussions
+  has_projects                = each.value.has_projects
+  has_wiki                    = each.value.has_wiki
+  is_template                 = each.value.is_template
+  allow_merge_commit          = each.value.allow_merge_commit
+  allow_squash_merge          = each.value.allow_squash_merge
+  allow_rebase_merge          = each.value.allow_rebase_merge
+  allow_auto_merge            = each.value.allow_auto_merge
+  squash_merge_commit_title   = each.value.squash_merge_commit_title
+  squash_merge_commit_message = each.value.squash_merge_commit_message
+  merge_commit_title          = each.value.merge_commit_title
+  merge_commit_message        = each.value.merge_commit_message
+  delete_branch_on_merge      = each.value.delete_branch_on_merge
+  auto_init                   = each.value.auto_init
+  archived                    = each.value.archived
+  archive_on_destroy          = each.value.archive_on_destroy
+  topics                      = each.value.topics
+
+  dynamic "pages" {
+    for_each = {
+      for value in local.pages_repos_list : value => value if value == each.value.name
+    }
+    content {
+      source {
+        branch = each.value.pages.branch
+        path   = each.value.pages.path
+      }
+    }
+  }
+}
---
+++ terraform/github-repo/variables.tf
@@ -0,0 +1,126 @@
+variable "standard_github_repositories" {
+  description = "Map of objects describing managed non-template Github repositories."
+  type = list(object({
+    name               = string
+    description        = string
+    visibility         = string
+    homepage_url       = string
+    gitignore_template = string
+    license_template   = string
+    topics             = set(string)
+    pages              = map(string)
+  }))
+}
+
+variable "has_issues" {
+  description = "Set to true to enable the GitHub Issues features on the repository."
+  type        = bool
+  default     = false
+}
+
+variable "has_discussions" {
+  description = "Set to true to enable GitHub Discussions on the repository."
+  type        = bool
+  default     = false
+}
+
+variable "has_projects" {
+  description = <<EOF
+  "Set to true to enable the GitHub Projects features on the 
+  repository. Per the GitHub documentation when in an
+  organization that has disabled repository projects it will
+  default to false and will otherwise default to true. 
+  If you specify true when it has been disabled it will 
+  return an error."
+  EOF
+  type        = bool
+  default     = false
+}
+
+variable "has_wiki" {
+  description = "Set to true to enable the GitHub Wiki features on the repository."
+  type        = bool
+  default     = false
+}
+
+variable "is_template" {
+  description = "Set to true to tell GitHub that this is a template repository."
+  type        = bool
+  default     = false
+}
+
+variable "allow_merge_commit" {
+  description = "Set to false to disable merge commits on the repository."
+  type        = bool
+  default     = true
+}
+
+variable "allow_squash_merge" {
+  description = "Set to false to disable squash merges on the repository."
+  type        = bool
+  default     = true
+}
+
+variable "allow_rebase_merge" {
+  description = "Set to false to disable rebase merges on the repository."
+  type        = bool
+  default     = true
+}
+
+variable "allow_auto_merge" {
+  description = "Set to true to allow auto-merging pull requests on the repository."
+  type        = bool
+  default     = false
+}
+
+variable "squash_merge_commit_title" {
+  description = "Can be PR_TITLE or COMMIT_OR_PR_TITLE for a default squash merge commit title."
+  type        = string
+  default     = "COMMIT_OR_PR_TITLE"
+}
+
+variable "squash_merge_commit_message" {
+  description = "Can be PR_BODY, COMMIT_MESSAGES, or BLANK for a default squash merge commit message."
+  type        = string
+  default     = "BLANK"
+}
+
+variable "merge_commit_title" {
+  description = "Can be PR_TITLE or MERGE_MESSAGE for a default merge commit title."
+  type        = string
+  default     = "MERGE_MESSAGE"
+}
+
+variable "merge_commit_message" {
+  description = "Can be PR_BODY, PR_TITLE, or BLANK for a default merge commit message."
+  type        = string
+  default     = "BLANK"
+}
+
+variable "delete_branch_on_merge" {
+  description = "Automatically delete head branch after a pull request is merged."
+  type        = bool
+  default     = true
+}
+
+variable "auto_init" {
+  description = "Set to true to produce an initial commit in the repository."
+  type        = bool
+  default     = false
+}
+
+variable "archived" {
+  description = <<EOF
+  "Specifies if the repository should be archived. Defaults to false.
+  NOTE Currently, the API does not support unarchiving."
+  EOF
+  type        = bool
+  default     = false
+}
+
+variable "archive_on_destroy" {
+  description = "Set to true to archive the repository instead of deleting on destroy."
+  type        = bool
+  default     = false
+}
+
