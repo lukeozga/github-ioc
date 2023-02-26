c4c209e1bb43e51bf7011591bf68ce98080e4367
Lukasz Ozga <lukaszozga1324@gmail.com>
2023-02-26 17:51:24 +0000

    Remove unnecessary outputs.tf files

--- terraform/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "ssh_key" {
-  value = {
-    "id"  = module.github_user_ssh_key.ssh_key[*].id
-    "url" = module.github_user_ssh_key.ssh_key[*].url
-  }
-}
--- terraform/user-ssh-key/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "ssh_key" {
-  value = {
-    "id"  = github_user_ssh_key.public_ssh_key[*].id
-    "url" = github_user_ssh_key.public_ssh_key[*].url
-  }
-}
