e8b76e104250a4b2192d207c6e1ee885ee440c13
Lukasz Ozga <lukaszozga1324@gmail.com>
2023-02-26 18:02:05 +0000

    Remove unnecessry Github provider specification from root module's provider.tf file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,3 @@
 terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
   required_version = "~> 1.2.0"
-}
-
-provider "github" {
-  token = var.github_token != null ? var.github_token : null
 }
